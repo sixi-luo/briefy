c9ee35194aee809eb81f241ca40d6935432da479
sixi-luo <54471859+sixi-luo@users.noreply.github.com>
2026-02-11 22:32:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,9 @@ async def get_audio(date: str):
 
 
 def main():
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    import os
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 if __name__ == "__main__":
